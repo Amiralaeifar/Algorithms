931983a2905ec5a4c1dfcc408dd67cfc8ef1bcb4
amiralaeifar <amplusir@gmail.com>
2023-07-21 22:29:43 +0330

    limit an array with given max and min values

--- Algorithms/Arrays/limit.py
+++ Algorithms/Arrays/limit.py
@@ -12,10 +12,23 @@
     Complexity = O(n)
 '''
 
+# First solution
 def limit(arr, min=None, max=None):
     min_check = lambda val: True if min is None else (val >= min)
     max_check = lambda val: True if max is None else (val <= max)
     
     return [val for val in arr if min_check(val) and max_check(val)]
 
-print(limit([1, 2, 3, 4, 5], min=3))
+
+# Second solution 
+def limit(arr, min=None, max=None):
+    if len(arr) == 0:
+        return arr
+    
+    if min is None:
+        min = min(arr)
+        
+    if max is None:
+        max = max(arr)
+        
+    return list(filter(lambda val: min <= val <= max, arr))
