22b615d2b814b1cbe358a5860b988f0540b5c4a1
amiralaeifar <amplusir@gmail.com>
2023-07-28 02:36:42 +0330

    recursive sum

--- Algorithms/Recursive/recursive_sum.py
+++ Algorithms/Recursive/recursive_sum.py
@@ -5,5 +5,5 @@ def recursive_sum(arr):
     else:
         return arr[0] + recursive_sum(arr[1:])
     
-print( recursive_sum([4, 3, 1, 2]) )
+print( recursive_sum([4, 3, 1, 2]) ) # --> outputs 10
 
