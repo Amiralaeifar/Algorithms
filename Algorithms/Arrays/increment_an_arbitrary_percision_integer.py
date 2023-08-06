fdabbdca6696f93b871f4f6d13232278871066ae
amiralaeifar <amplusir@gmail.com>
2023-08-07 00:53:17 +0330

    another implementation avoiding overflow issue

--- Algorithms/Arrays/increment_an_arbitrary_percision_integer.py
+++ Algorithms/Arrays/increment_an_arbitrary_percision_integer.py
@@ -26,3 +26,26 @@ def increment_precision(arr):
 
 
 print( increment_precision([9, 9, 9]) )
+
+
+
+#another implementation 
+
+def increment_precision2(arr):
+    arr[-1] += 1
+    for i in reversed(range(1, len(arr))):
+        if arr[i] != 10:
+            break
+        arr[i] = 0
+        arr[i - 1] += 1
+        
+    if arr[0] == 10:
+        #There is a carry-out
+        arr[0] = 1
+        arr.append(0)
+    return arr
+
+
+print( increment_precision2([1, 2, 9]) )
+        
+        
