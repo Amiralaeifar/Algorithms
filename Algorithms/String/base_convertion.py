241f5bf6895b8e4f415411feb19fc9cca9cdfe5c
amiralaeifar <amplusir@gmail.com>
2023-08-17 15:10:22 +0330

    modified

--- Algorithms/String/base_convertion.py
+++ Algorithms/String/base_convertion.py
@@ -8,6 +8,8 @@ import string
 
 
 def convert_base(num_as_string, b1, b2):
+    
+    #converting a base 10 normal integer to another base number, representing in string.
     def construct_from_base(num_as_int, base):
         return ('' if num_as_int == 0 else
                 construct_from_base(num_as_int // base, base) + 
@@ -15,10 +17,13 @@ def convert_base(num_as_string, b1, b2):
         
         
     is_negative = num_as_string[0] == '-'
+    
+    #converting num_as_string to a base 10 normal integer
     num_as_int = functools.reduce(
         lambda running_sum, c: running_sum * b1 + string.hexdigits.index(c.lower()),
         num_as_string[is_negative:], 0
     )
+    
     return ('-' if is_negative else '') + ('0' if num_as_int == 0 else
                                            construct_from_base(num_as_int, b2))
     
